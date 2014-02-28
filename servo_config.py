ff8ba31f4037434787481e2d84d4f3c918bd34b5
Riley Chapman <rlychpmn@gmail.com>
2014-02-28 01:44:39 -0500

    Adding first files

---
+++ servo_config.py
@@ -0,0 +1,10 @@
+import math
+ 
+servo_param = {
+    # Pan Servo
+    11: { 
+        'home_encoder': 430,
+        'max_ang': math.radians( 141.0 ),
+        'min_ang': math.radians( -31.0 )
+       }
+}
