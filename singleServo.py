25a0626e54d91943b4f3df3cd62ea2ceac198b78
Riley Chapman <rlychpmn@gmail.com>
2014-04-10 18:04:08 -0400

    single servo with linux

--- singleServo.py
+++ singleServo.py
@@ -1,11 +1,13 @@
 from lib_robotis import *
 
+
 def to_angle(angle):
 	"""
 	Move to angle (radians)
 	"""
 
-	dyn = USB2Dynamixel_Device('/dev/tty.usbserial-A9GZZXHD') #setup usb controller
+	#dyn = USB2Dynamixel_Device('/dev/tty.usbserial-A9GZZXHD') #setup usb controller
+	dyn = USB2Dynamixel_Device('/dev/ttyUSB0')
 	servo = Robotis_Servo(dyn, 11, 'MX') #setup servo object
 	servo.write_id(11) #choose a servo id of our liking
 	servo.move_angle(angle,blocking = False)       #(servo id, desired angle, speed, blocking)
