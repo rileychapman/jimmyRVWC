a24d3b52285fb323feeeed8def3c8f8185a9e810
Riley Chapman <rlychpmn@gmail.com>
2014-04-10 17:58:51 -0400

    adding IMU script

---
+++ serialAccel.py
@@ -0,0 +1,34 @@
+import time
+import serial
+import string
+
+def filter(line):
+	numbed = []
+	line = line.translate(string.maketrans("\n\r", "  "))
+	line = line.strip()
+	line = line.split(',')
+
+	for num in line:
+		if num == '':
+			numbed.append(0)
+		else:
+			num = float(num)
+			numbed.append(num)
+
+	return numbed
+	
+if __name__ == '__main__':
+	ser = serial.Serial('/dev/tty.usbmodem1411', 9600)
+	while True:
+		lines = []
+		for i in range(0,2):
+			lines.append(ser.readline())
+		#print lines
+		result = []
+		#for line in lines:
+		result.append(filter(lines[1]))
+		print result
+	
+
+
+
