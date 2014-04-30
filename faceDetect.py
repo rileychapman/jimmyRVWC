b1d77b29533ee39082fb996451cac9f95c262165
Riley Chapman <rlychpmn@gmail.com>
2014-04-30 11:21:02 -0400

    updating

--- faceDetect.py
+++ faceDetect.py
@@ -25,7 +25,11 @@ def detect_faces(image):
 if __name__ == "__main__":
     #cv.NamedWindow("Video", cv.CV_WINDOW_AUTOSIZE)
 
+    cv.SetCaptureProperty(cv.CaptureFromCAM(CAMERA_INDEX), 15,0)
     capture = cv.CaptureFromCAM(CAMERA_INDEX)
+    #capture = cv.CaptureFromCAM(CAMERA_INDEX)
+    #capture.set(CV_CAP_PROP_AUTO_EXPOSURE, 0 );
+    #print cv.GetCaptureProperty(capture, 15)
     storage = cv.CreateMemStorage()
     cascade = cv.Load(HAAR_CASCADE_PATH)
     faces = []
@@ -45,7 +49,7 @@ if __name__ == "__main__":
             cv.Rectangle(image, (face.x,face.y), (face.x+face.w,face.y+face.h), 255)
         if len(faces) != 0:
             face = faces[0]        
-            print "top face at:" + str(face.x) + ", "+ str(face.y)
+            #print "top face at:" + str(face.x) + ", "+ str(face.y)
 
         cv.ShowImage("w1", image)
 
